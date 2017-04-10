22d8481e593a780045a7acb1c92c25c7ed0af77f
bijan102 <bfazeli1@gmail.com>
2017-04-09 20:04:45 -0700

    Update: Implemented a few of the necessary functions to carry out adding a new entry to the DB.

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -10,23 +10,25 @@
 #include <iostream>
 #include <string.h>
 #include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
 
 using namespace std;
 
-// CONST FIELDS, 
-	// ID for entries, will be incr everytime a new entry is added
-	// ROW for the amount of entries the student DB will hold
-int iD = 1000, entries = 0;
+// CONST FIELDS
 const int ROW = 20;
+const char* ws = " \t\n\r\f\v";
 
+// ID for entries, will be incr everytime a new entry is added
+// ROW for the amount of entries the student DB will hold
+int iD = 1000, entries = 0;
 
 // 2D array of students to serve as Database
 string **students;
-students = new string*[ROW];
+students = new string*[ROW]();
 
 // Declarations of fnxs
 // addEntry
-bool addEntry(const string& fullName, const string& age, const string& year, const string& major, const string& gpa);
+int addEntry(const string& fullName, const string& age, const string& year, const string& major, const string& gpa);
 
 // deleteEntry
 bool deleteEntry(int target);
@@ -46,6 +48,9 @@ string getMajor();
 // getGPA
 string getGPA();
 
+// Trim from end of string (right)
+inline string& rtrim(std::string& s, const char* t = ws);
+
 // Main fnx
 int main()
 {
@@ -60,30 +65,30 @@ int main()
 	// Display Main Menu
 	bool isDone = false;
 	int mainMenuChoice = 0;
-	string fullName, age, year, major, gpa;
 
 	do
 	{
-		cout << "Main Menu:\n"
-			<< "1. Add a record\n"
-			<< "2. Delete a record\n"
-			<< "3. Display all records\n"
-			<< "4. Filter records by field value\n"
-			<< "5. Quit" << endl;
-		
 		do
 		{
+			cout << "Main Menu:\n"
+				<< "1. Add a record\n"
+				<< "2. Delete a record\n"
+				<< "3. Display all records\n"
+				<< "4. Filter records by field value\n"
+				<< "5. Quit" << endl;
+
+			cin >> mainMenuChoice;
+
 			if (mainMenuChoice < 0 || mainMenuChoice > 5)
 			{
 				cout << "Please enter a value between 1 - 5"
-					<< "\nas displayed on the menu." << endl;
+					<< "\nas displayed on the menu.\n" << endl;
 			}
-			cin >> mainMenuChoice;
 		} while (mainMenuChoice < 0 || mainMenuChoice > 5);
 
 		if (mainMenuChoice == 1)	// Add entry
 		{
-			
+			cout << addEntry(getName(), getAge(), getYear(), getMajor(), getGPA()) << endl;
 		}
 		else if (mainMenuChoice == 2)
 		{
@@ -95,4 +100,132 @@ int main()
 
 	system("pause");
 	return 0;
+}
+
+// Definition of fnxs
+// addEntry
+int addEntry(const string& fullName, const string& age, const string& year, const string& major, const string& gpa)
+{
+	for (int i = 0; i < ROW; ++i)
+	{
+		students[i][0] =  ++iD;
+		students[i][1] = fullName;
+		students[i][2] = age;
+		students[i][3] = year;
+		students[i][4] = major;
+		students[i][5] = gpa;
+	}
+	return iD;
+}
+
+// deleteEntry
+bool deleteEntry(int target)
+{
+	for (int i = 0; i < ROW; ++i)
+	{
+		if (students[i][0] == target)
+		{
+			students[i][0] = "";
+			students[i][1] = "";
+		}
+		sf
+	}
+}
+
+// getName
+string getName()
+{
+	string fullName;
+
+	do
+	{
+		cout << "Enter the student's name: ";
+		cin.getline(fullName);
+
+		if ((rtrim(fullName)) == "")
+		{
+			cout << "Field can not be left blank." << endl;
+		}
+	} while (fullName == "");
+
+	return fullName;
+}
+
+// getAge
+string getAge()
+{
+	string age;
+	do
+	{
+		cout << "Enter the student's age: ";
+		cin >> age;
+
+		if ((rtrim(age)) == "")
+		{
+			cout << "Field can not be left blank." << endl;
+		}
+	} while (age == "");
+
+	return age;
+}
+
+// getYear
+string getYear()
+{
+	string year;
+	do
+	{
+		cout << "Enter the student's current year: ";
+		cin >> year;
+
+		if ((rtrim(year)) == "")
+		{
+			cout << "Field can not be left blank." << endl;
+		}
+	} while (year == "");
+
+	return year;
+}
+
+// getMajor
+string getMajor()
+{
+	string major;
+	do
+	{
+		cout << "Enter the student's current major: ";
+		cin >> year;
+
+		if ((rtrim(major) == "")
+		{
+			cout << "Field can not be left blank." << endl;
+		}
+	} while (major == "");
+
+	return major;
+}
+
+// getGPA
+string getGPA()
+{
+	string gpa;
+	do
+	{
+		cout << "Enter the student's current gpa: ";
+		cin >> gpa;
+
+		if ((rtrim(gpa)) == "")
+		{
+			cout << "Field can not be left blank." << endl;
+		}
+	} while (gpa == "");
+
+	return gpa;
+}
+
+// Trim
+inline std::string& rtrim(string& s, const char* t = ws)
+{
+	s.erase(s.find_last_not_of(t) + 1);
+	return s;
 }
